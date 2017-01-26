d9d5699b455f8c2949e25849bd5a1749517f705e
Jake Kelly <email": null>
2017-01-26 11:59:08 -0800

    Style links as buttons; create destroy, fix update form and route

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -3,11 +3,14 @@ class RobotsController < ApplicationController
   def index
     @inventory_robots = Robot.where("inventory = true").order("created_at DESC")
     @ordered_robots = Robot.where("inventory = false").order("created_at DESC")
-    # Person.where("administrator = 1").order("created_on DESC").find(1)
   end
 
   def show
-    @robot = Robot.find(params[:id])
+    if Robot.exists?(params[:id])
+      @robot = Robot.find(params[:id])
+    else
+      @robot = nil
+    end
   end
 
   def edit
@@ -17,10 +20,16 @@ class RobotsController < ApplicationController
   def update
     @robot = Robot.find(params[:id])
     if @robot.update_attributes({inventory: params[:inventory], designation: params[:designation]})
-      redirect_to robot_path(@robot.id), :notice => "Your robot's status has been updated"
+      redirect_to robot_path(@robot.id), :destroyed => "Your robot's status has been updated"
     else
       render "edit"
     end
+  end
+
+  def destroy
+    robot = Robot.find(params[:id])
+    Robot.destroy(robot)
+    redirect_to robot_path(robot), :notice => "Robot has been destroyed"
   end
 
   # private
