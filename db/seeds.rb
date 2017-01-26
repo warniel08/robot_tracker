c1a0f1e0601f38d7b4852e2dfe18b2a8718c8b8f
Warner Nielsen <bwn888@gmail.com>
2017-01-26 13:53:12 -0800

    Change seed file height and weight for Model, updat manufacturer show page to create link

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,6 @@ Manufacturer.create(name: "Warner's World Inc")
 5.times {Robot.create(designation: "HAL", inventory: false, model_id: 1)}
 5.times {Robot.create(designation: "Ava", inventory: true, model_id: 2)}
 
-Model.create(model_designation: "RX111", height: 12.2, weight: 2.3, manufacturer_id: (Manufacturer.first.id))
-Model.create(model_designation: "RX112", height: 3.5, weight: 1.1, manufacturer_id: (Manufacturer.first.id))
-Model.create(model_designation: "RX113", height: 8.0, weight: 3.7, manufacturer_id: (Manufacturer.first.id))
+Model.create(model_designation: "RX111", height: 2.5, weight: 2.3, manufacturer_id: (Manufacturer.first.id))
+Model.create(model_designation: "RX112", height: 3.7, weight: 1.1, manufacturer_id: (Manufacturer.first.id))
+Model.create(model_designation: "RX113", height: 3.1, weight: 3.7, manufacturer_id: (Manufacturer.first.id))
