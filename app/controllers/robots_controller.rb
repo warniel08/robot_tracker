9d74d1a62566f79631be9fc208e2a205ce21d8bd
Jake Kelly <email": null>
2017-01-25 21:12:05 -0800

    Add broken edit form, won't update 'designation' field

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -10,4 +10,22 @@ class RobotsController < ApplicationController
     @robot = Robot.find(params[:id])
   end
 
+  def edit
+    @robot = Robot.find(params[:id])
+  end
+
+  def update
+    @robot = Robot.find(params[:id])
+    if @robot.update_attributes({inventory: params[:inventory], designation: params[:designation]})
+      redirect_to robot_path(@robot.id), :notice => "Your robot's status has been updated"
+    else
+      render "edit"
+    end
+  end
+
+  # private
+
+  #   def bot_params
+  #     params.require(:robot).permit(:designation, :inventory, :model_id)
+  #   end
 end
