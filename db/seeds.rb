02c27df37a0592095d72598afd21c9e5f33ff77c
Warner Nielsen <bwn888@gmail.com>
2017-01-26 14:37:24 -0800

    Add model and manufacturer variables to robot spec tests, change seed file to add robots correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@ Manufacturer.create(name: "Wayne Enterprises")
 Manufacturer.create(name: "Stark Industries")
 Manufacturer.create(name: "Warner's World Inc")
 
-5.times {Robot.create(designation: "HAL", inventory: false, model_id: 1)}
-5.times {Robot.create(designation: "Ava", inventory: true, model_id: 2)}
-
 Model.create(model_designation: "RX111", height: 2.5, weight: 2.3, manufacturer_id: (Manufacturer.first.id))
 Model.create(model_designation: "RX112", height: 3.7, weight: 1.1, manufacturer_id: (Manufacturer.first.id))
 Model.create(model_designation: "RX113", height: 3.1, weight: 3.7, manufacturer_id: (Manufacturer.first.id))
+
+5.times {Robot.create(designation: "HAL", inventory: false, model_id: Model.first.id)}
+5.times {Robot.create(designation: "Ava", inventory: true, model_id: Model.first.id)}
