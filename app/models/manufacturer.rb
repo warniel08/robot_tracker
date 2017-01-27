8cd7c57f3f2b8b6e5713a8fe1ceda2736569a602
Warner Nielsen <bwn888@gmail.com>
2017-01-27 10:28:33 -0800

    Add httparty to gemfile, write code in manufacturers model to pull the manufacturers info

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,6 +1,13 @@
+require 'httparty'
+require 'json'
+
 class Manufacturer < ApplicationRecord
   has_many :models
   has_many :robots, through: :models
 
   validates :name, presence: true
+
+  response = HTTParty.get("http://jordankamin.com/robots_api/robots.json")
+  data = JSON.parse(response.body)
+  p data
 end
