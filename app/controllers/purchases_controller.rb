aec33e9629b9e29e35406ba8488306d68b060b2c
Warner Nielsen <bwn888@gmail.com>
2017-01-28 13:48:43 -0800

    Update purchases show page and controller

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,6 +8,17 @@ class PurchasesController < ApplicationController
   end
 
   def show
+    if Robot.exists?(params[:id])
+      @robot = Robot.find(params[:id])
+    else
+      @robot = nil
+    end
+  end
+
+  def destroy
+    robot = Robot.find(params[:id])
+    Robot.destroy(robot)
+    redirect_to purchases_path(robot), :notice => "Order has been canceled"
   end
 
 end
