3009bb34aea1300bc9a2238aa34b48e1193d6133
Jeffrey Kranking <email": null>
2017-01-28 14:20:10 -0800

    fixes seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ User.create(username: 'the Commissioner', email: 'commissioner@gmail.com', passw
 Manufacturer.generate_manufacturer_names
 Model.generate_models_data
 
-16.times { Robot.create(designation: "Unnamed", model_id: Model.order("RANDOM()").first.id) }
+16.times { Robot.create(designation: "Unnamed", model_id: Model.order("RANDOM()").first.id, user_id: User.first.id) }
 
-16.times { Robot.create(designation: Faker::Name.name, inventory: true, model_id: Model.order("RANDOM()").first.id) }
+16.times { Robot.create(designation: Faker::Name.name, inventory: true, model_id: Model.order("RANDOM()").first.id, user_id: User.first.id) }
+
